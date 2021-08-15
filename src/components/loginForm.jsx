c2912cb683c70be31140be18aa6007a0729ae2c1
Dar <Levy>
2021-08-15 10:23:50 +0300

    Add bootstrap's login template

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Call the server
+    console.log("Submitted");
+  };
+
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div>
+        <h1 className="mb-4">Login</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Email address</label>
+            <input
+              className="form-control"
+              id="username"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
